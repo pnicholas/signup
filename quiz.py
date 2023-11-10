c3877c417a819f58ae81d2d849684bfb94ed255e
pnbc <peter.bjerregaard@gmail.com>
2023-11-10 10:45:47 +0100

    Team name generator & timestamp added

--- quiz.py
+++ quiz.py
@@ -1,7 +1,10 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 import os
 from config import DATABASE_URI
+from random_names import get_random_team_name
+from datetime import datetime
+import pytz
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URI
@@ -9,13 +12,14 @@ db = SQLAlchemy(app)
 
 class Team(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    team_name = db.Column(db.String(80), nullable=False)
+    team_name = db.Column(db.String(80), nullable=False)    
     e_mail = db.Column(db.String(255), nullable=False)
     participant_1 = db.Column(db.String(80))
     participant_2 = db.Column(db.String(80))
     participant_3 = db.Column(db.String(80))
     participant_4 = db.Column(db.String(80))
     participant_5 = db.Column(db.String(80))
+    timestamp = db.Column(db.DateTime, default=datetime.utcnow)
 
 @app.route('/')
 def index():
@@ -37,6 +41,11 @@ def signup():
         return "Error: E-mail is required."
     
     if len(participants) > 0:
+        # Convert to local timezone using pytz
+        timestamp_utc = datetime.utcnow()
+        local_tz = pytz.timezone('Europe/Copenhagen')
+        timestamp_local = timestamp_utc.replace(tzinfo=pytz.utc).astimezone(local_tz)
+
         new_team = Team(
             team_name=team_name,
             e_mail=e_mail,
@@ -44,13 +53,19 @@ def signup():
             participant_2=participants[1] if len(participants) >= 2 else None,
             participant_3=participants[2] if len(participants) >= 3 else None,
             participant_4=participants[3] if len(participants) >= 4 else None,
-            participant_5=participants[4] if len(participants) >= 5 else None
+            participant_5=participants[4] if len(participants) >= 5 else None,
+            timestamp=timestamp_local
         )
         db.session.add(new_team)
         db.session.commit()
         return "Successfully signed up!"
     else:
         return "Error: Invalid team size. You must at least have one member on your team."
+
+@app.route('/random_team_name')
+def random_team_name():
+    random_name = get_random_team_name()
+    return jsonify({'random_team_name': random_name})
 
 if __name__ == '__main__':
     with app.app_context():
